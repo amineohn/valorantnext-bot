bf3567c2fccd6e1b27720ca07d75e36b34c07340
amineohn <amineprojet7@gmail.com>
2022-10-21 15:53:28 +0200

    wip: emoji detecting ranks.

--- src/commands/match.ts
+++ src/commands/match.ts
@@ -6,11 +6,12 @@ import {
 } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 import Valorant, { Regions } from "unofficial-valorant-api";
+import { mmrHistoryType } from "../types/mmrHistory";
 @Discord()
 export class MatchHistory {
   @Slash({
     description: "Historique des matchs",
-    name: "match",
+    name: "history-ranked",
   })
   async MatchHistory(
     @SlashOption({
@@ -26,7 +27,7 @@ export class MatchHistory {
       required: true,
       type: ApplicationCommandOptionType.String,
     })
-    name: string,
+    named: string,
     @SlashOption({
       description: "Riot Tag",
       name: "riot-tag",
@@ -38,6 +39,7 @@ export class MatchHistory {
   ): Promise<void> {
     const api = new Valorant();
     if (command.deferred || command.replied) return;
+    const name = named.replace(/%20/g, " ");
     api.getMMRHistory({ name, tag, region }).then((mmr: any) => {
       console.log(mmr);
       const formatedDate = (date: string) => {
@@ -46,10 +48,59 @@ export class MatchHistory {
           dateObj.getMonth() + 1
         }/${dateObj.getFullYear()} à ${dateObj.getHours()}:${dateObj.getMinutes()}`;
       };
-
       new Pagination(
         command,
-        mmr.data.map((match: any) => {
+        mmr.data.map((match: mmrHistoryType) => {
+          if (match.currenttier === null) {
+            command.reply(
+              "Vous n'avez pas encore joué de matchs classés sur cette région."
+            );
+            return;
+          }
+          let emoji = "";
+          switch (match.currenttier) {
+            case "iron":
+              match.currenttier = "Fer";
+              emoji = "<:iron:879100202100469800>";
+              break;
+            case "bronze":
+              match.currenttier = "Bronze";
+              emoji = "<:bronze:879100202100469800>";
+              break;
+            case "silver":
+              match.currenttier = "Argent";
+              emoji = "<:silver:879100202100469800>";
+              break;
+            case "gold":
+              match.currenttier = "Or";
+              emoji = "<:gold:879100202100469800>";
+              break;
+
+            case "Gold 1":
+              match.currenttier = "Or 1";
+              emoji = "<:gold:879100202100469800>";
+              break;
+            case "platinum":
+              match.currenttier = "Platine";
+              emoji = "<:platinum:879100202100469800>";
+              break;
+            case "diamond":
+              match.currenttier = "Diamant";
+              emoji = "<:diamond:879100202100469800>";
+              break;
+            case "immortal":
+              match.currenttier = "Immortel";
+              emoji = "<:immortal:879100202100469800>";
+              break;
+            case "ascendant":
+              match.currenttier = "Ascendant";
+              emoji = "<:ascendant:879100202100469800>";
+              break;
+            case "radiant":
+              match.currenttier = "Radiant";
+              emoji = "<:radiant:879100202100469800>";
+              break;
+          }
           const embed = new EmbedBuilder()
             .setTitle("Historique des matchs")
             .setDescription(`**${match.currenttier}**`)
@@ -57,12 +108,12 @@ export class MatchHistory {
             .addFields([
               {
                 name: "Elo",
-                value: `${match.elo}`,
+                value: `:star: ${match.elo}`,
                 inline: true,
               },
               {
                 name: "Rang",
-                value: `${match.currenttierpatched}`,
+                value: `${emoji} ${match.currenttierpatched}`,
                 inline: true,
               },
               {
--- src/types/mmrHistory.ts
+++ src/types/mmrHistory.ts
@@ -1,16 +1,12 @@
 export type mmrHistoryType = {
-  data: [
-    {
-      currenttier: string;
-      currenttierpatched: string;
-      images: {
-        icon: string;
-        portrait: string;
-      };
-      mmr_change_to_last_game: number;
-      elo: number;
-      date: string;
-      date_raw: string;
-    }
-  ];
+  currenttier: string;
+  currenttierpatched: string;
+  images: {
+    icon: string;
+    portrait: string;
+  };
+  mmr_change_to_last_game: number;
+  elo: number;
+  date: string;
+  date_raw: string;
 };
